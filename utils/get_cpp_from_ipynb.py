63781e919b1b69190aac6e6744ea0ea8c2d54ee1
Kang Won LEE <kangwon.lee@kpu.ac.kr>
2018-09-26 10:33:45 +0900

    Started get_cpp_src_from_ipynb()

--- utils/get_cpp_from_ipynb.py
+++ utils/get_cpp_from_ipynb.py
@@ -1,4 +1,6 @@
 import os
+import nbformat
+import sys
 
 
 def has_gpp():
@@ -8,3 +10,17 @@ def has_gpp():
     result = os.system('bash which g++')
     print(f"has_gpp() result = {result}")
     return not (result)
+
+
+def get_cpp_src_from_ipynb(path):
+    with open(path) as ipynb:
+        nb = nbformat.read(ipynb, nbformat.NO_CONVERT)
+        print(nb)
+
+
+def main(arg):
+    get_cpp_src_from_ipynb(arg[0])
+
+
+if "__main__" == __name__:
+    main(sys.argv[1:])

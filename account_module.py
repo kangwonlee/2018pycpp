26c54d2c23bd767b96d701268b7dc5bc7fb0e026
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-10-02 01:17:56 -0700

    account_module.py

--- account_module.py
+++ account_module.py
@@ -1,11 +1,20 @@
+# Begin account_module.py
 balance = 0
 
+
 def deposit(amount):
+    global balance
     balance += amount
 
+
 def withdraw(amount):
+    global balance
     balance += (-amount)
 
+
 def check():
+    global balance
     return balance
 
+
+# End account_module.py
